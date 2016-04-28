0da0515eb65e049daa9cdaa326e2d7257b6d04b9
tinez7g <tinez7g@users.noreply.github.com>
2016-04-28 15:36:37 -0500

    Validación de token en métodos post

--- routes.js
+++ routes.js
@@ -9,6 +9,14 @@ router.get('*', function(req, res, next) {
   }
 });
 
+router.post('*', function(req, res, next) {
+  if (req.body.tkn != 1234) {
+    res.send('TKN INVALIDO');
+  }else{
+    next();
+  }
+});
+
 router.use('/api/root',require('./services/root'));
 router.use('/api/item',require('./services/item'));
 
